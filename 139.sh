5d48ed9bff6f8016875de05a0e7fbf2d45e7d996
Aviv <aviv.blitzer@gmail.com>
2026-02-23 03:27:28 +0200

    pushing page 139 solution

---
+++ 139.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+######################################
+# created by: Aviv 
+# purpose: Practice 139 solution
+# version: 0.0.1
+# date: 2026-02-23
+######################################
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#create the volume
+docker volume create mysql-data
+
+#run container with the volume mounted
+docker run -d --name mysql1 -e MYSQL_ROOT_PASSWORD=pass -v mysql-data:/var/lib/mysql mysql:8
+
+#kill the container
+docker rm -f mysql1
+
+#check that volume data persists
+docker volume inspect mysql-data
+
+#Run a new SQL cntainer with the same volume
+docker run -d --name mysql2 -e MYSQL_ROOT_PASSWORD=pass -v mysql-data:/var/lib/mysql mysql:8
+
+#Verify data is intact
+docker exec mysql2 ls /var/lib/mysql
